1c14ab1d1d95009fdfeaab55283361544cd359fc
Sneha Spandana <36546445+sneha-thammineni@users.noreply.github.com>
2018-06-19 01:11:23 +0530

    Add files via upload

---
+++ crud-express-mongo-master/public/main.js
@@ -0,0 +1,34 @@
+/* globals fetch */
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Sneha',
+      'quote': 'This is my first quote.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Sneha'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
